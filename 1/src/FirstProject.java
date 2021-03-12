8d6ed65b5f639f1f9e0431db45e561a1023e4f5c
neginkheirmand <neginkheirmand@gmail.com>
2021-03-12 21:03:21 +0330

    created project and added library

---
+++ 1/src/FirstProject.java
@@ -0,0 +1,21 @@
+public class FirstProject {
+    public static void main(String[] args) {
+    
+    }
+
+    private static boolean delete(String key){
+
+    }
+
+    private static boolean fetch(String key){
+
+    }
+
+    private static boolean update(String key, String value){
+
+    }
+
+    private static boolean create(String key, String value){
+
+    }
+}
