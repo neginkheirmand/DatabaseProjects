3e0e142373d5f54d6011597e00e9cabf38f296d1
neginkheirmand <neginkheirmand@gmail.com>
2021-05-31 22:36:16 +0430

    updated create user table query

--- 2 (Canary Project)/CREATE_User.sql
+++ 2 (Canary Project)/CREATE_User.sql
@@ -2,8 +2,10 @@ CREATE TABLE USER(
 Username VARCHAR(20) NOT NULL,
 Name VARCHAR(20) NOT NULL,
 Surname VARCHAR(20) NOT NULL,
-Pass VARCHAR(128) NOT NULL,
+Pass VARCHAR(40) NOT NULL, #since the value of the 128 character is passed throw a sha1 function the output will be of fixed lenght of 40
 Date_Of_Birth DATE NOT NULL,
-Date_Of_Sub DATE NOT NULL,
-Bio VARCHAR(64)
+SubTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+Bio VARCHAR(64),
+
+PRIMARY KEY (Username)
 );
