68466363f9c1d80c6172f647c5c624029e43bcba
neginkheirmand <neginkheirmand@gmail.com>
2021-07-14 14:30:34 +0430

    created follow some user procedure

---
+++ 2 (Canary Project)/part2/followSomeUserQuery.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE followSomeUser (Followed_ VARCHAR(20))
+BEGIN
+	DECLARE Follower_ VARCHAR(20);
+    SET Follower_ = twitter.GetLastLogIn();
+    # follow someone for the user if isent already following that person
+	INSERT INTO
+	  FOLLOWING_DATA (
+		Follower, Followed
+	  )
+	SELECT
+	  Follower_, Followed_
+	WHERE
+	  NOT EXISTS (SELECT * FROM FOLLOWING_DATA WHERE Follower = Follower_ AND Followed = Followed_);
+END
