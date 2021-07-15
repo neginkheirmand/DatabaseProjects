c46150ad2494eb8e40fb019954c49e7b2745ae11
neginkheirmand <neginkheirmand@gmail.com>
2021-07-15 23:00:14 +0430

    updated trigger

--- 2 (Canary Project)/part2/after_tweet_trigger_2_insert_hashtags.sql
+++ 2 (Canary Project)/part2/after_tweet_trigger_2_insert_hashtags.sql
@@ -1,5 +1,5 @@
 CREATE TRIGGER after_tweet_trigger_2_insert_hashtags
     AFTER INSERT ON Tweet
     FOR EACH ROW 
-call getHashtags(NEW.ID, NEW.Content)
+call getHashtags(NEW.Content, NEW.ID)
  # since its an 'after' trigger there is no need to validate the data
