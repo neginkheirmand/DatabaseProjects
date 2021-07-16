bd05a311d197ad78000b604b0941233d6c580ed4
neginkheirmand <neginkheirmand@gmail.com>
2021-07-16 15:44:10 +0430

    updated function

--- 2 (Canary Project)/part2/createOpinionAsTweet.sql
+++ 2 (Canary Project)/part2/createOpinionAsTweet.sql
@@ -1,9 +1,9 @@
-CREATE FUNCTION createOpinionAsTweet (Content_ varchar(256), parentTweetId int)
-RETURNS INTEGER
+CREATE DEFINER=`root`@`localhost` FUNCTION `createOpinionAsTweet`(Content_ varchar(256), parentTweetId int) RETURNS int
 BEGIN
 	#first validate the input parameters
 	DECLARE validation bool;
 	DECLARE user VARCHAR(20);
+	DECLARE tweeter VARCHAR(20);
     set validation = true;
     SET user = twitter.GetLastLogIn();
 
@@ -11,10 +11,18 @@ BEGIN
 		then select false into validation;
     end if;
     
+    #make sure data is valid
 	IF (validation is TRUE) THEN
-		INSERT INTO TWEET(Content, Username, ParentID)
-		VALUES 	(Content_, user, parentTweetId);
-		RETURN 1;
+		#make sure the tweet exists
+        set tweeter =  twitter.getTweeter(parentTweetId);
+        if not(tweeter = null) then
+        # make sure the tweeter did not block user
+			if (twitter.isUserBlocked(tweeter) = 0) then
+				INSERT INTO TWEET(Content, Username, ParentID)
+				VALUES 	(Content_, user, parentTweetId);
+				RETURN 1;
+			end if;
+		end if;
 	end IF;
 RETURN 0;
 END
