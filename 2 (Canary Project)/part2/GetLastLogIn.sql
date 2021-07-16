957182edab183a9b1124f28259762814d0b2f5ae
neginkheirmand <neginkheirmand@gmail.com>
2021-07-16 15:43:44 +0430

    fixed some bugs in getLastLogIN function

--- 2 (Canary Project)/part2/GetLastLogIn.sql
+++ 2 (Canary Project)/part2/GetLastLogIn.sql
@@ -1,11 +1,12 @@
-CREATE FUNCTION `GetLastLogIn` ()
-RETURNS VARCHAR(20)
+CREATE DEFINER=`root`@`localhost` FUNCTION `GetLastLogIn`() RETURNS varchar(20) CHARSET utf8mb4
+    READS SQL DATA
+    DETERMINISTIC
 BEGIN
 	 DECLARE lastUser VARCHAR(20);
 	 SELECT Username 
      FROM login 
      ORDER BY ID 
-     ASC LIMIT 1
+     DESC LIMIT 1
      into lastUser;
 
 RETURN lastUser;
