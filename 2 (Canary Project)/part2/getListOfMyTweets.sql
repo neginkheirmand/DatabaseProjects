c6b61f9d6064e8eeed9f5cd44a4537828151db97
neginkheirmand <neginkheirmand@gmail.com>
2021-07-16 16:17:41 +0430

    update getListOfMyTweets procedure

--- 2 (Canary Project)/part2/getListOfMyTweets.sql
+++ 2 (Canary Project)/part2/getListOfMyTweets.sql
@@ -3,6 +3,11 @@ BEGIN
 	#since the user is the last person to log in
     DECLARE user VARCHAR(20);
     SET user = GetLastLogIn();
+    
+    if user = null then 
+		select null;
+    end if;
+    
     select *
 	from tweet
 	where tweet.Username = user;
